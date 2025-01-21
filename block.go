23b87a3cf45e59f4c179358741421bcd8516b657
egaotan <chain_dev@163.com>
2025-01-21 16:38:08 +0800

    update

--- block.go
+++ block.go
@@ -16,7 +16,7 @@ func New() *Block {
 	return &Block{}
 }
 
-func (block *Block) Parse(slot uint64, b *rpc.GetParsedBlockResult, parsers map[solana.PublicKey]Parser) {
+func (block *Block) ParseWithParsers(slot uint64, b *rpc.GetParsedBlockResult, parsers map[solana.PublicKey]Parser) {
 	block.Slot = slot
 	block.Time = uint64(*b.BlockTime)
 	block.Hash = b.Blockhash
@@ -33,6 +33,31 @@ func (block *Block) Parse(slot uint64, b *rpc.GetParsedBlockResult, parsers map[
 			continue
 		}
 		err = myTx.ParseActions(parsers)
+		if err != nil {
+			// do nothing
+		}
+		myTxs = append(myTxs, myTx)
+	}
+	block.Transaction = myTxs
+}
+
+func (block *Block) Parse(slot uint64, b *rpc.GetParsedBlockResult) {
+	block.Slot = slot
+	block.Time = uint64(*b.BlockTime)
+	block.Hash = b.Blockhash
+	myTxs := make([]*Transaction, 0)
+	for i, tx := range b.Transactions {
+		myTx := NewTransaction()
+		err := myTx.Parse(&tx)
+		if err != nil {
+			continue
+		}
+		myTx.Seq = i + 1
+		if len(myTx.Instructions) == 0 {
+			// no instruction
+			continue
+		}
+		err = myTx.ParseActions(DefaultParse)
 		if err != nil {
 			// do nothing
 		}
